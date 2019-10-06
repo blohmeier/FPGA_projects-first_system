b0b9f55e3651c0a515027cba3883fe0a7876df68
Brett Lohmeier <brett.lohmeier@gmail.com>
2019-10-06 11:31:22 -0500

    Update first_system.v
    updated for programming the fpga on the basys3 board

--- first_system.v
+++ first_system.v
@@ -1,12 +1,34 @@
-module first_system(out1,out2,in1,in2);
-input in1,in2;
-output out1,out2;
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 10/05/2019 05:40:28 PM
+// Design Name: 
+// Module Name: first_system
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module first_system(led,sw);
+input [1:0]sw;
+output [1:0]led;
 
 wire and_out,or_out;
 
-assign and_out  = in1 & in2;
-assign or_out   = in1 | in2;
-assign out1     = and_out ^ or_out;
-assign out2     = ~ in2;
+assign and_out  = sw[0] & sw[1];
+assign or_out   = sw[0] | sw[1];
+assign led[0]     = and_out ^ or_out;
+assign led[1]     = ~ sw[1];
 
 endmodule
